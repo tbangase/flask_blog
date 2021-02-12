ed3b80ba85d0775b3b5b700090e7bc21e5f5815c
tbangase <ichi.toshi217@gmail.com>
2021-02-12 10:14:30 +0900

    Finish tutorial that make flaskr

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,3 +1,5 @@
+from flask.templating import render_template
+from flaskr.db import get_db
 import os
 from flask import Flask
 
@@ -27,5 +29,11 @@ def create_app(test_config=None):
     
     from . import db
     db.init_app(app)
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
